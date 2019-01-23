dc5fccfc6b38d0452735695748888131a337761a
Jacob Brown <kardeiz@gmail.com>
2019-01-23 09:30:21 -0600

    first commit

---
+++ examples/simple.rs
@@ -0,0 +1,35 @@
+#[macro_use]
+extern crate objekt_clonable;
+
+extern crate objekt;
+
+#[clonable]
+trait MyTrait: Clone {
+    fn recite(&self);
+}
+
+impl MyTrait for String {
+    fn recite(&self) {
+        println!("{} ♫", self);
+    }
+}
+
+#[derive(Clone)]
+struct Container {
+    trait_object: Box<MyTrait>,
+}
+
+fn main() {
+    let line = "The slithy structs did gyre and gimble the namespace";
+
+    // Build a trait object holding a String.
+    // This requires String to implement MyTrait and std::clone::Clone.
+    let x: Box<MyTrait> = Box::new(String::from(line));
+
+    x.recite();
+
+    // The type of x2 is a Box<MyTrait> cloned from x.
+    let x2 = objekt::clone_box(&*x);
+
+    x2.recite();
+}
---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+#![recursion_limit = "2048"]
+
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use proc_macro2::Span;
+use quote::quote;
+
+use syn::*;
+
+#[proc_macro_attribute]
+pub fn clonable(attrs: TokenStream, item: TokenStream) -> TokenStream {
+    let mut item_trait = parse_macro_input!(item as ItemTrait);
+
+    let item_trait_ident = &item_trait.ident;
+
+    let cloneish_paths: Vec<Path> = vec![
+        parse_quote!(Clone),
+        parse_quote!(std::clone::Clone),
+        parse_quote!(::std::clone::Clone),
+    ];
+
+    if let Some(path) = item_trait.supertraits.iter_mut()
+        .filter_map(|x| match x { TypeParamBound::Trait(ref mut y) => Some(y), _ => None })
+        .map(|x| &mut x.path )
+        .find(|x| cloneish_paths.iter().any(|y| &y == x) ) {
+        *path = parse_quote!(_objekt::Clone);     
+    } else {
+        panic!("`Clone` must be present in trait supertrait list");
+    }
+
+    (quote!{
+        extern crate objekt as _objekt;
+        use _objekt::{clone_trait_object, __internal_clone_trait_object};
+        #item_trait
+        clone_trait_object!(#item_trait_ident);
+    }).into()
+
+}
