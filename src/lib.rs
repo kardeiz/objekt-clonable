c973ce743cfeaff65fd4caea60ae5724e55144f3
Jacob Brown <kardeiz@gmail.com>
2019-01-23 12:23:21 -0600

    rename issues

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::quote;
 
 use syn::*;
@@ -12,6 +13,10 @@ pub fn clonable(_attrs: TokenStream, item: TokenStream) -> TokenStream {
     let mut item_trait = parse_macro_input!(item as ItemTrait);
 
     let item_trait_ident = &item_trait.ident;
+
+    let random = rand::random::<u64>();
+
+    let objekt_rename = &Ident::new(&format!("_objekt_{}", random), Span::call_site());
 
     let cloneish_paths: Vec<Path> = vec![
         parse_quote!(Clone),
@@ -29,14 +34,16 @@ pub fn clonable(_attrs: TokenStream, item: TokenStream) -> TokenStream {
         .map(|x| &mut x.path)
         .find(|x| cloneish_paths.iter().any(|y| &y == x))
     {
-        *path = parse_quote!(objekt::Clone);
+        *path = parse_quote!(#objekt_rename::Clone);
     } else {
         panic!("`Clone` must be present in trait supertrait list");
     }
 
+
     (quote!{
+        extern crate objekt as #objekt_rename;
         #item_trait
-        clone_trait_object!(#item_trait_ident);
+        clone_trait_object!(#item_trait_ident);        
     })
     .into()
 }
